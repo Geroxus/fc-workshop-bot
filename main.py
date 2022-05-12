5a661bd3fd49124a9828abfca63c531b4c42671e
Gero <gero.bardowicks@web.de>
2022-05-12 17:49:52 +0200

    boot message on conntected servers

--- main.py
+++ main.py
@@ -4,6 +4,12 @@ import discord
 class MyClient(discord.Client):
     async def on_ready(self):
         print('Logged on as {0}!'.format(self.user))
+        for guild in self.guilds:
+            for tc in guild.text_channels:
+                if tc.name.startswith('general'):
+                    await tc.send('Hello there!\n(This is an automatic message displayed, when I am booted.')
+                    break
+            print('logged onto: {0.name}'.format(guild))
 
     async def on_message(self, message):
         print('Message from {0.author}: {0.content}'.format(message))
